7c218dd60482c94030e6a5febef087a6f1cdc1d8
santanaIgot <santanaigoor01@gmail.com>
2024-10-09 20:32:43 -0300

    [Fix] - Instanciando novo objeto na controller

--- EventosMvc/src/main/java/br/com/cp/fiap/EventosMvc/controller/EventoController.java
+++ EventosMvc/src/main/java/br/com/cp/fiap/EventosMvc/controller/EventoController.java
@@ -3,20 +3,19 @@ package br.com.cp.fiap.EventosMvc.controller;
 import br.com.cp.fiap.EventosMvc.model.Evento;
 
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/evento")
-public class EventoCadastrar {
+public class EventoController {
 
     @GetMapping("/cadastrar")
-    public String cadastrar(){
+    public String cadastrar(Model model){
+        model.addAttribute("evento", new Evento());
         return "evento/cadastrar";
     }
 
