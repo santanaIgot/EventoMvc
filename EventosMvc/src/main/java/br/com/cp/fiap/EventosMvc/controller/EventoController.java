b6586ac5ba7b80bbf9c8e11089bcd84eb511c1c0
santanaIgot <santtanaigoor01@gmail.com>
2024-10-13 18:44:45 -0300

    [Feat] - Crud finalizado

--- EventosMvc/src/main/java/br/com/cp/fiap/EventosMvc/controller/EventoController.java
+++ EventosMvc/src/main/java/br/com/cp/fiap/EventosMvc/controller/EventoController.java
@@ -4,12 +4,16 @@ import br.com.cp.fiap.EventosMvc.model.Evento;
 
 import br.com.cp.fiap.EventosMvc.repository.EventoRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/evento")
@@ -38,5 +42,30 @@ public class EventoController {
     public String listar(Model model){
         model.addAttribute("evento", eventoRepository.findAll());
         return "evento/lista";
+    }
+
+    @GetMapping("editar/{id}")
+    public String Editar(@PathVariable("id") Long id, Model model){
+            model.addAttribute("evento", eventoRepository.findById(id));
+
+            return "evento/editar";
+    }
+
+
+    @PostMapping("editar")
+    @Transactional
+    public String Editar(@Valid Evento evento, Model model,
+                         RedirectAttributes redirectAttributes){
+        eventoRepository.save(evento);
+        redirectAttributes.addFlashAttribute("msg","evento atualizado");
+        return "redirect:/evento/lista";
+    }
+
+    @PostMapping("excluir")
+    @Transactional
+    public String excluir(Long idEvent, RedirectAttributes redirectAttributes){
+        eventoRepository.deleteById(idEvent);
+        redirectAttributes.addFlashAttribute("msg", "evento removido!");
+        return "redirect:/evento/lista";
     }
 }
--- EventosMvc/src/main/java/br/com/cp/fiap/EventosMvc/model/Evento.java
+++ EventosMvc/src/main/java/br/com/cp/fiap/EventosMvc/model/Evento.java
@@ -1,6 +1,10 @@
 package br.com.cp.fiap.EventosMvc.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,18 +18,33 @@ public class Evento {
 
     @Id
     @GeneratedValue
+    @NotNull
     private Long id;
+
+    @NotBlank(message = "Preencha o nome do evento ")
+    @Size(max = 50)
     @Column(name = "nm_nome", length = 50, nullable = false)
     private String nome;
+
+    @Size(min = 10 , max = 200)
+    @NotNull
     @Column(name = "ds_descricao", length = 50, nullable = false)
     private String descricao;
+
+    @NotNull
     @Column(name = "nm_atracao", nullable = false)
     private String atracao;
+
+    @NotNull
     @Column(name = "nm_localizacao", nullable = false)
     private String localizacao;
+
+    @NotNull
     @Column(name = "st_status", nullable = false)
     private Status status;
 
+    @Past
+    @NotNull
     @DateTimeFormat(pattern = "dd/MM/yy")
     @Column(name = "dt_data")
     private LocalDate data;
